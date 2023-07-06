1157bf8786167676ece7cb9bc12cd990d3ccb6da
marxlima <marx.lima@ssp.gov.ma.br>
2023-07-06 18:10:38 -0300

    first commit

---
+++ database/migrations/2023_07_05_232248_create_advertises_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Category;
+use App\Models\State;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advertises', function (Blueprint $table) {
+            $table->id();
+            $table->string('tilte');
+            $table->float('price');
+            $table->boolean('is_negotiable');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->unsignedBigInteger('state_id');
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advertises');
+    }
+};
